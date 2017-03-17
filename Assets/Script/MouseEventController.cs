737f11a30e172ae462e70b5aa0581dba6a7f5995
Hoa Nguyen <Hoa_Nguyen@thecdm.ca>
2017-03-17 15:32:07 -0700

    Add: Hover Event for the map elements

---
+++ Assets/Script/MouseEventController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class MouseEventController : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+	public Image description;
+
+	// Use this for initialization
+	void Start () {
+		description.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnPointerEnter(PointerEventData pointerEvent) {
+		description.enabled = true;
+	}
+
+	public void OnPointerExit(PointerEventData pointerEvent) {
+		description.enabled = false;
+	}
+}
